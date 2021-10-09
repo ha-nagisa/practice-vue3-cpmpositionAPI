8fa075a5c212835fc6b74818b183385b6a720ecd
ha-nagisa <nagisa18222@icloud.com>
2021-10-09 20:53:18 +0900

    practice compositionAPI

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Children from '@/views/Children'
+import TeleportTest from '@/views/TeleportTest'
+import CompositionTest from '@/views/CompositionTest'
+import PropsEmitTest from '@/views/PropsEmitTest'
+import FunctionTest from '@/views/FunctionTest'
+import RouterTest from '@/views/RouterTest'
+import VuexTest from '@/views/VuexTest'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/children',
+    name: 'Children',
+    component: Children
+  },
+  {
+    path: '/teleport-test',
+    name: 'TeleportTest',
+    component: TeleportTest
+  },
+  {
+    path: '/composition-test',
+    name: 'CompositionTest',
+    component: CompositionTest
+  },
+  {
+    path: '/props-emit-test',
+    name: 'PropsEmitTest',
+    component: PropsEmitTest
+  },
+  {
+    path: '/function-test',
+    name: 'FunctionTest',
+    component: FunctionTest
+  },
+  {
+    path: '/router-test',
+    name: 'RouterTest',
+    component: RouterTest
+  },
+  {
+    path: '/vuex-test',
+    name: 'VuexTest',
+    component: VuexTest
   },
   {
     path: '/about',
